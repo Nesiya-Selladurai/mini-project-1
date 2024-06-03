30dd0d36cb3baed5b7300d2bbabfd71aee9acf66
Nesiya-Selladurai <nesiyakavitha213@gmail.com>
2024-06-03 10:30:21 +0530

    Update PhnBook.c

--- PhnBook.c
+++ PhnBook.c
@@ -1,51 +1,90 @@
-#include <stdio.h>
+
+    #include <stdio.h>
 #include <string.h>
 
 #define MAX_CONTACTS 100
+#define MAX_NAME_LENGTH 50
+#define MAX_PHONE_LENGTH 15
 
-// Define a structure for storing contact information
+// Structure to represent a contact
 struct Contact {
-    char name[50];
-    char phone_number[15];
+    char name[MAX_NAME_LENGTH];
+    char phone[MAX_PHONE_LENGTH];
 };
 
+// Array to store contacts
+struct Contact contacts[MAX_CONTACTS];
+
+// Number of contacts currently stored
+int numContacts = 0;
+
 // Function prototypes
-void addContact(struct Contact contacts[], int *numContacts);
-void displayContacts(struct Contact contacts[], int numContacts);
+void addContact();
+void searchContact();
 
 int main() {
-    struct Contact contacts[MAX_CONTACTS];
-    int numContacts = 0;
-    char choice;
+    int choice;
 
     do {
-        printf("\nPhone Book Menu\n");
+        printf("\nPhone Book\n");
         printf("1. Add Contact\n");
-        printf("2. Display Contacts\n");
+        printf("2. Search Contact\n");
         printf("3. Exit\n");
         printf("Enter your choice: ");
-        scanf(" %c", &choice);
+        scanf("%d", &choice);
 
         switch (choice) {
-            case '1':
-                addContact(contacts, &numContacts);
+            case 1:
+                addContact();
                 break;
-            case '2':
-                displayContacts(contacts, numContacts);
+            case 2:
+                searchContact();
                 break;
-            case '3':
-                printf("Exiting the program.\n");
+            case 3:
+                printf("Exiting...\n");
                 break;
             default:
-                printf("Invalid choice. Please enter again.\n");
+                printf("Invalid choice. Please try again.\n");
         }
-    } while (choice != '3');
+    } while (choice != 3);
 
     return 0;
 }
 
-// Function to add a contact to the phone book
-void addContact(struct Contact contacts[], int *numContacts) {
-    if (*numContacts < MAX_CONTACTS) {
-        printf("\nEnter name: ");
-        scanf(" %[^\n]", contacts[*n
+// Function to add a contact
+void addContact() {
+    if (numContacts >= MAX_CONTACTS) {
+        printf("Phone book is full. Cannot add more contacts.\n");
+        return;
+    }
+
+    printf("Enter contact name: ");
+    scanf("%s", contacts[numContacts].name);
+    printf("Enter contact phone number: ");
+    scanf("%s", contacts[numContacts].phone);
+
+    printf("Contact added successfully.\n");
+    numContacts++;
+}
+
+// Function to search for a contact
+void searchContact() {
+    char searchName[MAX_NAME_LENGTH];
+    printf("Enter the name to search: ");
+    scanf("%s", searchName);
+
+    int found = 0;
+    for (int i = 0; i < numContacts; i++) {
+        if (strcmp(contacts[i].name, searchName) == 0) {
+            printf("Contact found:\n");
+            printf("Name: %s\n", contacts[i].name);
+            printf("Phone: %s\n", contacts[i].phone);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("Contact not found.\n");
+    }
+}
